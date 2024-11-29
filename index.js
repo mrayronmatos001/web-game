936673a70c905330e22020dfc0b534762d268647
Guilherme Felipe <guilhermefelipe007@gmail.com>
2024-11-29 13:28:41 -0300

    Collisions Update

--- index.js
+++ index.js
@@ -9,24 +9,61 @@ const h = canvas.height = 576;
 let mapWidth = -200;
 let mapHeight = -630;
 
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i+=54){
+    collisionsMap.push(collisions.slice(i, 54 + i))
+}
 
-ctx.fillStyle = "white";
-ctx.fillRect(0,0, w, h);
+class Boundary{
+    static width = 48
+    static height = 48
+    constructor({position}) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -295,
+    y: -130
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1742)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
 
 class Sprite {
-    constructor({position, map}){
+    constructor({position, velocity, map}){
         this.position = position
         this.map = map
     }
-    draw(){
+    
+    draw() {
         ctx.drawImage(this.map, this.position.x, this.position.y)
     }
 }
 
 const background = new Sprite({
     position: {
-        x: -200,
-        y: -630
+        x: offset.x,
+        y: offset.y
     },
      map: map
 })
@@ -47,9 +84,21 @@ const keys = {
 
 }
 
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+const movables = [background, testBoundary]
 function animate(){
     window.requestAnimationFrame(animate)
     background.draw()
+    //boundaries.forEach((boundary) =>{
+    //    boundary.draw()
+    //})
+    testBoundary.draw()
     ctx.drawImage(
         player,
         0,
@@ -61,10 +110,25 @@ function animate(){
         player.width/6,
         player.height
     )
-    if (keys.w.pressed && lastKey === 'w') background.position.y = background.position.y += 3
-    else if(keys.a.pressed && lastKey === 'a') background.position.x = background.position.x += 3
-    else if(keys.d.pressed && lastKey === 'd') background.position.x = background.position.x -= 3
-    else if(keys.s.pressed && lastKey === 's') background.position.y = background.position.y -= 3
+    //if (player.position.x + player.width)
+    if (keys.w.pressed && lastKey === 'w') {
+        movables.forEach((movable) => {
+            movable.position.y +=3
+        })
+    }else if(keys.a.pressed && lastKey === 'a') {
+        movables.forEach((movable) => {
+            movable.position.x +=3
+        })
+    }else if(keys.d.pressed && lastKey === 'd') {
+        movables.forEach((movable) => {
+            movable.position.x -=3
+        })
+    }
+    else if(keys.s.pressed && lastKey === 's') {
+        movables.forEach((movable) => {
+            movable.position.y -=3
+        })
+    }
 }
 animate()
 
